b4e5876a0b5d916aa9da476031006363738b3e23
SamaraMaciel <82781502+SamaraMaciel@users.noreply.github.com>
2025-03-23 17:49:16 -0600

    *** Local time flow update - Inserted a local timer that fetches the system time and updates the Date and Time fields every 1 sec - MainWindow.xaml.cs file

--- Retail_PointOfSales/MainWindow.xaml.cs
+++ Retail_PointOfSales/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.Json; // For System.Text.Json
-
+using System.ComponentModel;
 
 namespace Retail_PointOfSales
 {
@@ -19,29 +19,31 @@ namespace Retail_PointOfSales
     public partial class MainWindow : Window
     {
         private List<Product> _products;
-        
+        //Initializing the actual date and time and setting it to the actualDate variable
+        DateTime actualDate = DateTime.Now;
+     
         public MainWindow()
         {
             InitializeComponent();
             //MainContent.Content = new Login(); 
-            SetDate();
-            SetTime();
 
+            // Initializes a new instance of the Timer class that raises an event at every 1000 milliseconds (1 second)
+            var timer = new System.Timers.Timer(1000);
+            timer.Elapsed += dispatcherTimer_Tick; //Setting event handler for the timer interval
+            timer.Start(); // Startig the timer
         }
 
-        private void SetDate()
+        // Event handler for the timer interval
+        public void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            // Fetch the current date and time
-            DateTextBox.Text = DateTime.Now.ToString("MM/dd/yyyy");  //For modification not fetching real date
-            
+            actualDate = DateTime.Now;
+            // Updating the Date and Time in the UI - Putting it in the Dispatcher thread that runs in the UI
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                DateTextBox.Text = actualDate.ToString("MM/dd/yyyy");
+                TimeTextBox.Text = actualDate.ToString("HH:mm:ss");
+            });
         }
-        private void SetTime()
-        {
-            // Fetch the current date and time
-            TimeTextBox.Text = DateTime.Now.ToString("hh:mm tt"); //For modification not fetching real time
-        }
-
-        
 
         // Search Text Box from the JSON File
         private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
