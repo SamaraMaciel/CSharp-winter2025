4de29fc0b6740ff20bb57ab9ffc91b129e6ced43
SamaraMaciel <82781502+SamaraMaciel@users.noreply.github.com>
2025-03-29 00:51:03 -0600

    **** Main window "total" field formatted after delete or void actions **** Inserted validation for delete action with an empty list **** Inserted validation for delete action without selecting an item **** Void event handler created **** Void action needs confirmation from the user before voiding the sale **** Inserted validation for void action when list was already empty **** EndOfDay update: "Cash sales" field is returning filtered data from sales.jon based on today's date **** Inserted scrollbar in the search panel

--- Retail_PointOfSales/Model/SaleManager.cs
+++ Retail_PointOfSales/Model/SaleManager.cs
@@ -8,9 +8,12 @@ public class SaleManager
     string filePath = Path.Combine(AppContext.BaseDirectory, @"..\..\..\JSON\sales.json");
     List<Sale> sales = [];
 
+    private List<Sale> sales2 { get; set; } //List of Sale type that is being used in the LoadAllSales() method
+
     public SaleManager()
     {
         LoadSales();
+        LoadAllSales(); // loads the LoadAllSales() method when a new instance of the SaleManager class is created
     }
     
     public void LoadSales()
@@ -41,4 +44,22 @@ public class SaleManager
         Console.WriteLine(filteredSales.Count);
         return filteredSales;
     }
+
+    // Method that reads all sales from the sales.json file
+    public List<Sale> LoadAllSales()
+    {
+        //string filePath = Path.Combine(AppContext.BaseDirectory, @"..\..\..\JSON\sales.json");
+        List<Sale> sales2 = new List<Sale>();
+        if (File.Exists(filePath))
+        {
+            using StreamReader fileToRead = new StreamReader(filePath);
+            string json = fileToRead.ReadToEnd();
+            if (!string.IsNullOrEmpty(json))
+            {
+                sales2 = JsonConvert.DeserializeObject<List<Sale>>(json) ?? [];
+            }
+        }
+        return sales2;
+    }
+
 }
